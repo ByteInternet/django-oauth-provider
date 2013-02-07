33667086973b062d1bcc449631850b8bfe42e7f4
Michał Jaworski <swistakm@gmail.com>
2013-02-07 13:22:14 +0100

    fix example.provider.settings in order to run tests in newer versions of django

--- examples/provider/settings.py
+++ examples/provider/settings.py
@@ -3,6 +3,18 @@ ROOT_PATH = os.path.dirname(__file__)
 
 TEMPLATE_DEBUG = DEBUG = True
 MANAGERS = ADMINS = ()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'testdb.sqlite',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
 #DATABASE_ENGINE = 'sqlite3'
 #DATABASE_NAME = os.path.join(ROOT_PATH, 'testdb.sqlite')
 DATABASE_ENGINE = 'postgresql_psycopg2'
