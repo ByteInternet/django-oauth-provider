067dcd997d33d75572ac7787f9bcf58f4ee92e4e
Michał Jaworski <swistakm@gmail.com>
2013-04-04 10:48:30 +0200

    exception throwing less ambiguous

--- oauth_provider/decorators.py
+++ oauth_provider/decorators.py
@@ -1,4 +1,4 @@
-from oauth2 import Error
+import oauth2
 
 try:
     from functools import update_wrapper
@@ -42,18 +42,18 @@ class CheckOAuth(object):
                 token = store.get_access_token(request, oauth_request, 
                                 consumer, oauth_request.get_parameter('oauth_token'))
             except InvalidTokenError:
-                return send_oauth_error(Error(_('Invalid access token: %s') % oauth_request.get_parameter('oauth_token')))
+                return send_oauth_error(oauth2.Error(_('Invalid access token: %s') % oauth_request.get_parameter('oauth_token')))
             try:
                 self.validate_token(request, consumer, token)
-            except Error, e:
+            except oauth2.Error, e:
                 return send_oauth_error(e)
             
             if self.resource_name and token.resource.name != self.resource_name:
-                return send_oauth_error(Error(_('You are not allowed to access this resource.')))
+                return send_oauth_error(oauth2.Error(_('You are not allowed to access this resource.')))
             elif consumer and token:
                 return self.view_func(request, *args, **kwargs)
         
-        return send_oauth_error(Error(_('Invalid request parameters.')))
+        return send_oauth_error(oauth2.Error(_('Invalid request parameters.')))
 
     @staticmethod
     def is_valid_request(request):
