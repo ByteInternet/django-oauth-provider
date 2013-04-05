8435446cb67bac5db9576af4b5c2dced247ba5d7
Michał Jaworski <swistakm@gmail.com>
2013-04-05 12:57:07 +0200

    we don't need admin in runtests

--- oauth_provider/runtests/urls.py
+++ oauth_provider/runtests/urls.py
@@ -1,13 +1,7 @@
 from oauth_provider.compat import url, patterns, include
-
-from django.contrib import admin
-
 from oauth_provider.views import protected_resource_example
-
-admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^oauth/', include('oauth_provider.urls')),
     url(r'^oauth/photo/$', protected_resource_example, name='oauth_example'),
-    url(r'^admin/', include(admin.site.urls)),
 )
